c9b2d92aa1b52f5be6a86d59e63b92d80b81cb00
Sean Champ <spchamp@users.noreply.github.com>
2019-09-19 00:52:22 -0700

    sbcl-git: Add build options; Update Makefile and PLIST for latest src
    sbcl-git/Makefile: Add texinfo source files to SUBST_FILES; add
    anotation about renamed patch-revert patch (does not patch) for builds
    on NetBSD; Include new options.mk; Update to add SBCL_BUILD_OPTIONS
    semantics, per the new options.mk; Update build semantics for optional
    documentation build; Remove new sbcl.old and sbcl.core.old during install
    
    sbcl-git/PLIST: Update for new upstream contrib components (sb-capstone,
    sb-mpfr, and uiop vis. ASDF); Update for installing new sbcl.mk file;
    Update for new documentation option
    
    sbcl-git/distinfo: Updating patches
    
    sbcl-git/options.mk: New; By-in-large documented; Some items TBD
    
    sbcl-git/patch-ab: Updated for new src
    
    sbcl-git/patch-revert*: Renamed, to prevent automatic patching (does not
    patch; TBD test the unpatched build under NetBSD)

---
+++ sbcl-git/options.mk
@@ -0,0 +1,95 @@
+
+PKG_OPTIONS_VAR=	PKG_OPTIONS.sbcl
+
+PKG_SUPPORTED_OPTIONS=	doc xref wtimer thruption safepoint threads zcore
+PKG_SUGGESTED_OPTIONS=	doc threads
+
+## TBD: options help for these esoteric build-option symbols
+
+.include "../../mk/bsd.options.mk"
+
+PLIST_VARS+=		doc
+
+## --
+
+.if !empty(PKG_OPTIONS:Mdoc)
+##
+## TBD: doc-pdf feature + texi2pdf or makeinfo --pdf
+##
+USE_TOOLS+=	makeinfo
+PLIST.doc=	yes
+.endif
+
+
+## Advanced port build options for SBCL 
+##
+## The following variables will affect SBCL_BUILD_OPTIONS:
+##   SBCL_BUILD_WITH_OPTIONS
+##   SBCL_BUILD_WITHOUT_OPTIONS
+
+
+.if !empty(PKG_OPTIONS:Mxref)
+SBCL_BUILD_WITH_OPTIONS+=	sb-xref-for-internals
+.else
+SBCL_BUILD_WITHOUT_OPTIONS+=	sb-xref-for-internals
+.endif
+
+## NB: Each of the following entails a dependency on the subsequent,
+## when enabled in the build.
+##
+## This is implemented in reference to the SBCL source file,
+## base-target-features.lisp-expr
+##
+##
+## TBD - Parsing PKG_OPTIONS.sbcl in order to verify that the 
+## set of provided options will not conflict with the following 
+## configuration logic -- furthermore, emitting an error message, 
+## in case of conflicting options
+##
+## e.g +witmer -threads [untenable]
+##
+
+.if !empty(PKG_OPTIONS:Mwtimer)
+. if empty(PKG_OPTIONS:Mthruption)
+PKG_OPTIONS+=thruption
+. endif
+SBCL_BUILD_WITH_OPTIONS+=	sb-wtimer
+.else 
+SBCL_BUILD_WITHOUT_OPTIONS+=	sb-wtimer
+.endif
+
+
+.if !empty(PKG_OPTIONS:Mthruption)
+. if empty(PKG_OPTIONS:Msafepoint)
+PKG_OPTIONS+=safepoint
+. endif
+SBCL_BUILD_WITH_OPTIONS+=	sb-thruption
+.else 
+SBCL_BUILD_WITHOUT_OPTIONS+=	sb-thruption
+.endif
+
+
+.if !empty(PKG_OPTIONS:Msafepoint)
+. if empty(PKG_OPTIONS:Mthreads)
+PKG_OPTIONS+=threads
+. endif
+SBCL_BUILD_WITH_OPTIONS+=	sb-safepoint
+.else 
+SBCL_BUILD_WITHOUT_OPTIONS+=	sb-safepoint
+.endif
+
+
+.if !empty(PKG_OPTIONS:Mthreads)
+SBCL_BUILD_WITH_OPTIONS+=	sb-thread
+.else 
+SBCL_BUILD_WITHOUT_OPTIONS+=	sb-thread
+.endif
+
+
+.if !empty(PKG_OPTIONS:Mzcore)
+## TBD: May affect PLIST
+SBCL_BUILD_WITH_OPTIONS+=	sb-core-compression
+.include "../../devel/zlib/buildlink3.mk"
+.else 
+SBCL_BUILD_WITHOUT_OPTIONS+=	sb-core-compression
+.endif
