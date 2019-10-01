88543092096c84ac41725f81a87b32da3041c6b2
Sean Champ <spchamp@users.noreply.github.com>
2019-09-30 19:19:07 -0700

    wip/emacs-git: Use the same symbol as PKGBASE in buildlink3.mk

--- emacs-git/buildlink3.mk
+++ emacs-git/buildlink3.mk
@@ -1,15 +1,16 @@
-# $NetBSD: buildlink3.mk,v 1.1 2013/05/07 12:04:59 makoto Exp $
+# $NetBSD$
 
-BUILDLINK_TREE+=	emacs
+BUILDLINK_TREE+=	emacs27
 
 .if !defined(EMACS_BUILDLINK3_MK)
 EMACS_BUILDLINK3_MK:=
 
-.include "../../editors/emacs/modules.mk"
-BUILDLINK_API_DEPENDS.emacs+=	${_EMACS_REQD}
-BUILDLINK_PKGSRCDIR.emacs?=	${_EMACS_PKGDIR}
+.include "../../mk/bsd.prefs.mk"
+.include "../../${EMACS_BASE:Uwip/emacs-git}/modules.mk"
+BUILDLINK_API_DEPENDS.emacs27+=	${_EMACS_REQD}
+BUILDLINK_PKGSRCDIR.emacs27?=	${_EMACS_PKGDIR}
 
-BUILDLINK_CONTENTS_FILTER.emacs=	${EGREP} '.*\.el$$|.*\.elc$$'
+BUILDLINK_CONTENTS_FILTER.emacs27=	${EGREP} '.*\.el$$|.*\.elc$$'
 .endif # EMACS_BUILDLINK3_MK
 
-BUILDLINK_TREE+=	-emacs
+BUILDLINK_TREE+=	-emacs27
